e11226651a4a1824f4459adea11605197c5e2cc4
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2019-09-07 18:03:04 +0200

    Add files via upload

--- 2019_CITapp_stats_exp2.R
+++ 2019_CITapp_stats_exp2.R
@@ -6,93 +6,57 @@ library("TOSTER")
 # COLLECT DATA ----
 
 setwd(path_neat("data"))
+file_names = list.files(pattern = "^CIT_Mobile_app_exp2.*txt$")
 
-file_names_app = list.files(pattern = "^CIT_Mobile_app.*txt$")
-file_names_pc = list.files(pattern = "^CIT_Mobile_pc.*txt$")
-if (length(file_names_app) != length(file_names_pc)) {
-    print(paste(
-        "length not OK! app",
-        length(file_names_app),
-        "vs. pc",
-        length(file_names_pc)
-    ))
-    stop("file number inconsistent")
-}
-
-subj_nums = c()
-for (filee in file_names_app) {
-    subj_nums = c(subj_nums, strsplit(filee, "_")[[1]][4])
-}
-
-if (exists("main_cit_merg")) {
+if ( exists("main_cit_merg") ) {
     rm(main_cit_merg)
     rm(dems_merg)
+    rm(all_raw_data)
 }
 
-for (subject_number in subj_nums) {
-    #subject_number = "4"
-    #subject_number = "19"
-    print(subject_number)
+for(f_name in file_names){
+    #f_name = "CIT_Mobile_app_exp2_1_1_1_1_1547033034378.txt"
+ 
+    print(f_name)
     
-    pc_l = list.files(pattern = paste("^CIT_Mobile_pc", subject_number, ".*txt$", sep =
-                                          "_"))
-    subj_data_pc = read.table(
-        pc_l[1] ,
+    subj_data = read.table(
+        f_name,
         sep = "\t",
         header = TRUE,
         fill = TRUE,
         quote = "\""
     )
-    subj_data_pc$device_status = 0
-    
-    app_l = list.files(pattern = paste("^CIT_Mobile_app", subject_number, ".*txt$", sep =
-                                           "_"))
-    subj_data_app = read.table(
-        app_l[1] ,
-        sep = "\t",
-        header = TRUE,
-        fill = TRUE,
-        quote = "\""
-    )
-    subj_data_app$device_status = 1
-    
-    subj_data = merge(subj_data_pc, subj_data_app, all = T)
     
     subj_data$stim_type <- as.character(subj_data$stim_type)
     subj_data$stim_type[grepl('^irrelevant', subj_data$stim_type)] = "irrelevant"
     subj_data$stim_type <- factor(subj_data$stim_type)
     
     subj_itms_base = subj_data[which(
-        subj_data$block_number %in% list(4, 5) &
-            subj_data$stim_type %in% list(
-                "probe",
-                "irrelevant",
-                "selfrefitem",
-                "otherrefitem",
-                "target"
-            )
-    ),]
+        subj_data$block_number %in% list(4, 5, 7, 8) &
+            subj_data$stim_type %in% list("probe", "irrelevant", "selfrefitem", "otherrefitem", "target")
+    ), ]
     
     
-    if (nrow(subj_itms_base) != 432) {
-        # just double-check
+    if (nrow(subj_itms_base) != 432) { # just double-check
         print("number of rows:")
         print(nrow(subj_itms_base))
         stop("trial num incorrect")
     }
-    all_unique_names = subj_itms_base[!duplicated(subj_itms_base$stimulus_shown), ]
-    if (nrow(all_unique_names) != 12) {
-        # just double-check
-        print("number of unique names:")
-        print(nrow(all_unique_names))
-        stop("name problem")
-    }
     
+    
+    # all_unique_names = subj_itms_base[!duplicated(subj_itms_base$stimulus_shown), ]
+    # all_unique_names = all_unique_names[all_unique_names$stim_type == "probe", ]
     # if ( exists("exp_unique_names") ) {
     #     exp_unique_names =  merge( exp_unique_names, all_unique_names, all = T)
     # } else {
     #     exp_unique_names = all_unique_names
     # }
+    #xx = exp_unique_names[ , c("subject_id","condition","stimulus_shown", "date_in_ms")] 
+    #xx = dcast(setDT( xx ), subject_id ~ rowid(subject_id, prefix = "name"), value.var = c("stimulus_shown","condition","date_in_ms" ) )
+    
+    #xx = reshape(xx, idvar=c("subject_id"), direction="wide")
+    
+    
     
     # temp_raw = na.omit(subj_data, cols=c("incorrect"))
     # if ( exists("all_raw_data") ) {
@@ -100,8 +64,8 @@ for (subject_number in subj_nums) {
     # } else {
     #     all_raw_data = temp_raw
     # }
-    # all_raw_data = subset(all_raw_data, select=-c(stimulus_shown,date_in_ms,categ_order))
-    # write.table(all_raw_data, "C:/research/proj_citapp/exp1/full_results_exp1_sp_vs_pc.txt", quote = F, row.names = F, sep="\t")
+    # all_raw_data = subset(all_raw_data, select=-c(stimulus_shown,date_in_ms))
+    # write.table(all_raw_data, "C:/research/proj_citapp/exp2/full_results_exp2_index_vs_thumb.txt", quote = F, row.names = F, sep="\t")
     
     subj_itms_base$valid_trial = ifelse(
         subj_itms_base$incorrect == 0 &
@@ -111,16 +75,19 @@ for (subject_number in subj_nums) {
         0
     )
     subj_itms_base <-
-        subj_itms_base[order(subj_itms_base$block_number,
-                             subj_itms_base$trial_number),]
-    subj_cond = as.character(subj_itms_base$device_status[1])
+        subj_itms_base[order(subj_itms_base$block_number, subj_itms_base$trial_number), ]
+    subj_cond = as.character(subj_itms_base$condition[1])
+    subject_number = as.character(subj_itms_base$subject_id[1])
+    
+    subj_itms_base$handposition = ifelse(subj_itms_base$handposition == "Zeigefinger", 0, 1)
+    # "Zeigefinger" - 0; "Daumen" - 1
     
     
     subj_acc_rates = neatStats::aggr_neat(
         dat = subj_itms_base,
         values = valid_trial,
         method = mean,
-        group_by = c("stim_type", "device_status"),
+        group_by = c("stim_type", "handposition"),
         filt = (rt_start >= 150),
         prefix = "acc_rate"
     )
@@ -129,7 +96,7 @@ for (subject_number in subj_nums) {
         dat = subj_itms_base,
         values = rt_start,
         method = mean,
-        group_by = c("stim_type", "device_status"),
+        group_by = c("stim_type", "handposition"),
         filt = (rt_start >= 150 & valid_trial == 1),
         prefix = "rt_mean"
     )
@@ -142,7 +109,7 @@ for (subject_number in subj_nums) {
         dat = subj_itms_base,
         values = hold_dur,
         method = mean,
-        group_by = c("stim_type", "device_status"),
+        group_by = c("stim_type", "handposition"),
         filt = (rt_start >= 150 & valid_trial == 1),
         prefix = "dur_mean"
     )
@@ -162,41 +129,32 @@ for (subject_number in subj_nums) {
                               condition = subj_cond,
                               subject_line)
     
-    dems_l = list.files(pattern = paste(
-        "^CIT_Mobile_demographics",
-        subject_number,
-        ".*txt$",
-        sep =
-            "_"
-    ))
+    dems_string = subj_data[startsWith(as.character(subj_data$subject_id), 'dems'),]$condition
+    dems = c(subject_number, strsplit(as.character(dems_string), "/")[[1]])
+    if (length(dems) == 9) {
+        dems = c(dems, "na", "na")
+    }
+    dems = do.call(rbind.data.frame, list(dems))
+    colnames(dems) = c(
+        "subject_id",
+        "gender",
+        "age",
+        "rep1",
+        "rep2",
+        "rep3",
+        "rep4",
+        "dcit",
+        "version",
+        "length",
+        "width"
+    )
     
-    demographics = read.table(dems_l[1],
-                              sep = "\t",
-                              header = FALSE,
-                              fill = TRUE)
-    
-    colnames(demographics) <-
-        c(
-            "subject_id",
-            "gender",
-            "age",
-            "rat1",
-            "rat2",
-            "rat3",
-            "rat4",
-            "time",
-            "hand",
-            "country",
-            "email"
-        )
-    
-    if (exists("main_cit_merg")) {
-        # add subject aggregations
-        main_cit_merg =  merge(main_cit_merg, subject_line, all = T)
-        dems_merg = merge(dems_merg, demographics, all = T)
+    if ( exists("main_cit_merg") ) { # add subject aggregations
+        main_cit_merg =  merge( main_cit_merg, subject_line, all = T)
+        dems_merg = merge( dems_merg, dems, all = T)
     } else {
         main_cit_merg = subject_line
-        dems_merg = demographics
+        dems_merg = dems
     }
 }
 
@@ -210,20 +168,15 @@ main_cit_merg$dur_mean_diffs_1 = main_cit_merg$dur_mean_probe_1 -
 main_cit_merg$acc_rate_diffs_1 = main_cit_merg$acc_rate_probe_1 - main_cit_merg$acc_rate_irrelevant_1
 main_cit_merg$main_overall_acc = ((main_cit_merg$overall_acc_probe) + main_cit_merg$overall_acc_irrelevant * 4) / 5
 
+
 dems_merg$subject_id = as.character(dems_merg$subject_id)
-main_cit_merg = merge(x = main_cit_merg,
-                      y = dems_merg,
-                      by = "subject_id",
-                      all = TRUE)
 
+main_cit_merg = merge(x = main_cit_merg, y = dems_merg, by = "subject_id", all = TRUE)
 
-main_cit_data <-
-    main_cit_merg[order(main_cit_merg$condition, main_cit_merg$subject_id), ]
-main_cit_data <-
-    main_cit_merg[order(main_cit_merg$condition, main_cit_merg$subject_id), ]
+main_cit_data = main_cit_merg[order(main_cit_merg$condition, main_cit_merg$subject_id),]
 
-main_cit_data = main_cit_data[main_cit_data$overall_acc_target > 0.5,]
-main_cit_data = main_cit_data[main_cit_data$main_overall_acc > 0.75,]
+main_cit_data = main_cit_data[ main_cit_data$overall_acc_target > 0.5, ]
+main_cit_data = main_cit_data[ main_cit_data$main_overall_acc > 0.75, ]
 
 main_cit_merg$remaining = ifelse(main_cit_merg$subject_id %in% main_cit_data$subject_id, 1, 0)
 
@@ -235,7 +188,7 @@ excluded_nums = do.call(data.frame,
                                 sd = sd(x)
                             )))
 for (i in 1:nrow(excluded_nums)) {
-    row <- excluded_nums[i,]
+    row <- excluded_nums[i, ]
     print(paste(
         'for Condition:',
         row[1],
@@ -250,14 +203,11 @@ full_data = main_cit_data
 
 # demographics
 neatStats::dems_neat(full_data, percent = F)
-#
+
 
 # ANALYSIS ----
 
-
-print(
-    "::::::::::::::::::::::::::::::CONDITION - PC (0) or PHONE (1)::::::::::::::::::::::::::::::::::::"
-)
+print("::::::::::::::::::::::::::::::CONDITION - Index (0) or Thumb (1)::::::::::::::::::::::::::::::::::::")
 
 neatStats::t_neat(full_data$rt_mean_probe_0,
                   full_data$rt_mean_irrelevant_0,
@@ -289,6 +239,7 @@ neatStats::t_neat(full_data$dur_mean_diffs_0,
                   full_data$dur_mean_diffs_1,
                   pair = T)
 
+
 dataTOSTpaired(
     data = full_data,
     pairs = list(c(i1 = "rt_mean_diffs_0", i2 = "rt_mean_diffs_1")),
@@ -311,8 +262,6 @@ dataTOSTpaired(
     alpha = 0.05,
     plots = TRUE
 )
-
-names(full_data)
 
 
 neatStats::anova_neat(
@@ -340,61 +289,42 @@ neatStats::plot_neat(
     within_ids = list(
         device = c("_0", "_1"),
         p_vs_i = c("_probe", "_irrel")
-    )
+    ),
+    reverse = T
 )
 
 
+corr_neat(full_data$rt_mean_diffs_0,  full_data$dur_mean_diffs_0 )
+corr_neat(full_data$rt_mean_diffs_0,  full_data$acc_rate_diffs_0 )
 
+data_screen_size = full_data[!(full_data$length == "na"), ]
+data_screen_size$length = as.numeric( as.character( data_screen_size$length ) )
+data_screen_size$width = as.numeric( as.character(  data_screen_size$width ) )
 
-print("alertness per condition:")
-for (dvice_stat in list("0", "1")) {
-    # 0,1,2,3,4,5
-    rt_mean = full_data[[paste("rt_mean_diffs", dvice_stat, sep = "_")]]
-    acc = full_data[[paste("acc_rate_diffs", dvice_stat, sep = "_")]]
-    hold_rt = full_data[[paste("dur_mean_diffs", dvice_stat, sep = "_")]]
-    wakefulness = (full_data$rat1 + full_data$rat3) / 2
-    concentration =  (full_data$rat2 + full_data$rat4) / 2
-    general_alertness = (wakefulness + concentration) / 2
-    print(mean(general_alertness))
-    print(sd(general_alertness))
-    print(
-        "::::::::::::::::::::::::::::::CONDITION - PC (0) or PHONE (1)::::::::::::::::::::::::::::::::::::"
-    )
-    print(dvice_stat)
-    print("###")
-    print("RT mean:")
-    corr_neat(general_alertness, rt_mean)
-    print("Acc:")
-    corr_neat(general_alertness, acc)
-    print("Hold:")
-    corr_neat(general_alertness, hold_rt)
-}
+corr_neat(data_screen_size$rt_mean_diffs_0,  data_screen_size$width )
+corr_neat(data_screen_size$rt_mean_diffs_0,  data_screen_size$length )
+corr_neat(data_screen_size$rt_mean_diffs_0,  data_screen_size$width * data_screen_size$length )
+corr_neat(data_screen_size$rt_mean_diffs_1,  data_screen_size$width )
+corr_neat(data_screen_size$rt_mean_diffs_1,  data_screen_size$length )
+corr_neat(data_screen_size$rt_mean_diffs_1,  data_screen_size$width * data_screen_size$length )
 
-corr_neat(full_data$rt_mean_diffs_0,
-          full_data$dur_mean_diffs_0)
-corr_neat(full_data$rt_mean_diffs_0,  full_data$acc_rate_diffs_0)
-corr_neat(full_data$rt_mean_diffs_1,
-          full_data$dur_mean_diffs_1)
-corr_neat(full_data$rt_mean_diffs_1,  full_data$acc_rate_diffs_1)
-
+corr_neat(data_screen_size$acc_rate_diffs_0,  data_screen_size$width )
+corr_neat(data_screen_size$acc_rate_diffs_0,  data_screen_size$length )
+corr_neat(data_screen_size$acc_rate_diffs_0,  data_screen_size$width * data_screen_size$length )
+corr_neat(data_screen_size$acc_rate_diffs_1,  data_screen_size$width )
+corr_neat(data_screen_size$acc_rate_diffs_1,  data_screen_size$length )
+corr_neat(data_screen_size$acc_rate_diffs_1,  data_screen_size$width * data_screen_size$length )
 
 # SAVE -----
 
-# full_data = subset(full_data, select=-c(subj_coh_d_cit_0,subj_coh_d_cit_1,time,hand,country,email))
+# full_data = subset(full_data, select=-c(subj_coh_d_cit_0,subj_coh_d_cit_1,rep1,rep2,rep3,rep4))
 
-full_data <- full_data[order(as.integer(subject_id)), ]
+full_data <- full_data[order(as.integer(full_data$subject_id)),]
 
-write.table(
-    full_data,
-    path_neat("results_aggregated_exp1_sp_vs_pc.txt"),
-    quote = F,
-    row.names = F,
-    sep = "\t"
-)
+# write.table(full_data, "results_aggregated_exp2_index_vs_thumb.txt", quote = F, row.names = F, sep="\t")
 
 
-
-# final summary ----
+## final summary ----
 
 main_results = table_neat(
     list(
