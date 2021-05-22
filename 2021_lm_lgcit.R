2dc1d80771b8e5373ac8fb7c682b6cb200730a2b
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2021-05-22 21:33:33 +0200

    lm lgcit winner

--- 2021_lm_lgcit.R
+++ 2021_lm_lgcit.R
@@ -6,7 +6,7 @@ library('lme4')
 setwd(path_neat())
 
 lgcit_dat = readRDS("lgcit_guilty_both.rds")
-
+lgcit_dat$trial_number = lgcit_dat$trial_number/100
 str(lgcit_dat)
 
 #length(unique(lgcit_dat$subject_id))
@@ -19,55 +19,63 @@ str(lgcit_dat)
 #mlm_fulmer = robustlmm::rlmer(
 #mlm_fulmer = lmer(
 
-mlm_fulmer = robustlmm::lmerNoFit(
+
+mlm_full = lmer(
   rt_start ~ stim_type + block_number + trial_number +
     stim_type:block_number + stim_type:trial_number +
     (stim_type | subject_id),
-  data = lgcit_dat
+  data = lgcit_dat, REML = FALSE
 )
 
-mlm_full = glmer(
-  rt_start ~ stim_type + block_number + trial_number +
-    stim_type:block_number + stim_type:trial_number +
-    (stim_type | subject_id),
-  data = lgcit_dat,
-  family = Gamma(link = "identity")
-)
+# mlm_full3 = glmer(
+#   rt_start ~ stim_type + block_number + trial_number +
+#     stim_type:block_number + stim_type:trial_number +
+#     (stim_type | subject_id),
+#   data = lgcit_dat,
+#   family = Gamma(link = "identity")
+# )
 
-ggpubr::ggqqplot(resid(mlm_full), shape = 1)
-ggpubr::ggdensity(resid(mlm_full))
-lattice::xyplot(profile(mlm_fulmer))
-lattice::densityplot(profile(mlm_full))
-# lattice::splom(profile(mlm_full))
-plot(mlm_full, type = c("p", "smooth"))
-plot(mlm_full, sqrt(abs(resid(.))) ~ fitted(.),type = c("p", "smooth"))
+compp = performance::compare_performance(mlm_full, mlm_full9)
+compp
+plot(compp)
+
+####
+# performance::model_performance(mlm_full)
+# performance::check_model(mlm_full)
+# ggpubr::ggqqplot(resid(mlm_full), shape = 1)
+# ggpubr::ggdensity(resid(mlm_full))
+# lattice::xyplot(profile(mlm_fulmer))
+# lattice::densityplot(profile(mlm_full))
+# # lattice::splom(profile(mlm_full))
+# plot(mlm_full, type = c("p", "smooth"))
+# plot(mlm_full, sqrt(abs(resid(.))) ~ fitted(.),type = c("p", "smooth"))
 
 # confint(mlm_full, method = 'Wald')
 # summary(mlm_full)
 # report::report(mlm_full)
 
-mlm_xtrial = glmer(
+mlm_xtrial = lmer(
   rt_start ~ stim_type + block_number + trial_number +
     stim_type:block_number +
     (stim_type | subject_id),
-  data = lgcit_dat,
-  family = Gamma(link = "identity")
+  data = lgcit_dat, REML = FALSE
 )
+
 
 aov_trials = anova(mlm_full, mlm_xtrial)
 
-mlm_xblock = glmer(
+mlm_xblock = lmer(
   rt_start ~ stim_type + block_number + trial_number +
     stim_type:trial_number +
     (stim_type | subject_id),
-  data = lgcit_dat,
-  family = Gamma(link = "identity")
+  data = lgcit_dat, REML = FALSE
 )
 
 aov_blocks = anova(mlm_full, mlm_xblock)
 
 aov_trials
 aov_blocks
+
 
 ## plots
 model = mlm_full
